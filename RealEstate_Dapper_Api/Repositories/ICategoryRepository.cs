fb14aeb8072b05ca62c374d48a47812149b3a832
Cüneyt Kaya <kayacuneyt2542@gmail.com>
2023-08-07 13:20:47 +0300

    Add project files.

---
+++ RealEstate_Dapper_Api/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+
+namespace RealEstate_Dapper_Api.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResaultCategoryDto>> GetAllCategoryAsync();
+    }
+}
---
+++ RealEstate_Dapper_WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealEstate_Dapper_WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
